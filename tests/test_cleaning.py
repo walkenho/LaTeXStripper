afa1d1e7a232571513d94600494b8056d63917da
Jessica Walkenhorst <walkenho@users.noreply.github.com>
2022-01-22 18:26:10 +0000

    Add linting to tests (#8)

--- tests/test_cleaning.py
+++ tests/test_cleaning.py
@@ -2,15 +2,25 @@ from LaTeXStripper.LaTeXStripper import delete_braced_command, extract_body
 
 
 def test_delete_braced_command_deletes_braced_command():
-    assert (delete_braced_command('title', r'\begin{document}\n\title{Sample Title}\nSome text\n\end{document}')
-            == r'\begin{document}\n\nSome text\n\end{document}')
+    assert (
+        delete_braced_command(
+            "title", r"\begin{document}\n\title{Sample Title}\nSome text\n\end{document}"
+        )
+        == r"\begin{document}\n\nSome text\n\end{document}"
+    )
 
 
 def test_delete_braced_command_deletes_braced_command_with_option():
-    assert (delete_braced_command('email', r'text-part-1\n\email[some-fabulour-option]{email-adress}\ntext-part-2')
-            == r'text-part-1\n\ntext-part-2')
+    assert (
+        delete_braced_command(
+            "email", r"text-part-1\n\email[some-fabulour-option]{email-adress}\ntext-part-2"
+        )
+        == r"text-part-1\n\ntext-part-2"
+    )
 
 
 def test_extract_body_extracts_body():
-    assert (extract_body(r'\begin{document}\n\title{Sample Title}\nSome text\n\end{document}')
-            == r'\n\title{Sample Title}\nSome text\n')
+    assert (
+        extract_body(r"\begin{document}\n\title{Sample Title}\nSome text\n\end{document}")
+        == r"\n\title{Sample Title}\nSome text\n"
+    )
